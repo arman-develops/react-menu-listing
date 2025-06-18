a77e2fa8845d7ff3392418ed454685f6b2d32cd3
Amon Victor <amon.victor.develops@gmail.com>
2025-06-18 16:08:14 +0300

    Added the Listing component

---
+++ src/components/Listing.jsx
@@ -0,0 +1,37 @@
+
+function Listing() {
+    <>
+        <div>
+            {/* map listing items here */}
+        </div>
+    </>
+}
+
+function ListingItem() {
+    <>
+        <div>
+            <div>
+                <img src="" alt="" />
+                <div>
+                    {/* discount here */}
+                </div>
+                <div>
+                    {/* rating */}
+                </div>
+            </div>
+            <div>
+                <h6>
+                    {/* name here */}
+                </h6>
+                <p>
+                    {/* description */}
+                </p>
+                <p>
+                    {/* price */}
+                </p>
+            </div>
+        </div>  
+    </>
+}
+
+export default Listing
