56762b63867ce4fc47f11709749c9ced168c22ce
Amon Victor <amon.victor.develops@gmail.com>
2025-06-18 22:18:43 +0300

    refactored the header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,51 @@
+import { IoFastFoodOutline } from "react-icons/io5";
+import { RiDrinks2Line } from "react-icons/ri";
+import { IoPizzaOutline } from "react-icons/io5";
+import { LuSalad } from "react-icons/lu";
+import { GiCoolSpices } from "react-icons/gi";
+import { IoIceCreamOutline } from "react-icons/io5";
+
+const navItems = [
+    {
+        name: "All",
+        icon: <IoFastFoodOutline />
+    },
+    {
+        name: "Drinks",
+        icon: <RiDrinks2Line />
+    },
+    {
+        name: "Pizza",
+        icon: <IoPizzaOutline />
+    },
+    {
+        name: "Salad",
+        icon: <LuSalad />
+    },
+    {
+        name: "Spicy",
+        icon: <GiCoolSpices />
+    },
+    {
+        name: "Sweets",
+        icon: <IoIceCreamOutline />
+    }
+]
+
 function Header() {
     return (
         <>
             <div>
                 <div>
                     <nav>
-                        {/* Nav items map here */}
+                        <ul>
+                            {navItems.map(({name, icon}, index) => (
+                                <li key={index}>
+                                    {icon}
+                                    {name}
+                                </li>
+                            ))}
+                        </ul>
                     </nav>
                 </div>
                 <div>
