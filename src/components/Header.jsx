6f46960c3cb4e1dc539921deee5dd44b1952e987
Amon Victor <amon.victor.develops@gmail.com>
2025-06-18 15:41:01 +0300

    Added the Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+function Header() {
+    return (
+        <>
+            <div>
+                <div>
+                    <nav>
+                        {/* Nav items map here */}
+                    </nav>
+                </div>
+                <div>
+                    <a href="#">View All</a>
+                </div>
+            </div> 
+        </>
+    )
+}
+
+export default Header;
