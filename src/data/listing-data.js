f5fe076703f403a44954d7875e05c602dd865222
Amon Victor <amon.victor.develops@gmail.com>
2025-06-19 04:34:21 +0300

    Added listing data + images

---
+++ src/data/listing-data.js
@@ -0,0 +1,71 @@
+export const listingItems = [
+    {
+        discount: 20,
+        rating: 5.00,
+        img: "/veg-burger.png",
+        name: "Double Patty Veg Burger",
+        description: "A hearty, double-decker delight packed with fresh veggies, crunchy lettuce, and signature sauces.",
+        price: 40.00
+    },
+    {
+        discount: 30,
+        rating: 5.00,
+        img: "/chicken-burger.png",
+        name: "Chicken Burger",
+        description: "Juicy grilled chicken breast topped with melted cheese, lettuce, and a creamy mayo spread.",
+        price: 17.56
+    },
+    {
+        discount: 20,
+        rating: 4.00,
+        img: "/pinapple-pizza.png",
+        name: "Pineapple Pizza",
+        description: "A sweet and savory fusion of tropical pineapple chunks and cheesy goodness on a crispy crust.",
+        price: 25.50
+    },
+    {
+        discount: 8,
+        img: "/pineapple-soup.png",
+        name: "Pineapple Soup",
+        description: "A warm and tangy soup blending pineapple sweetness with subtle spices for a unique starter.",
+        price: 11.02
+    },
+    {
+        discount: 17,
+        rating: 3.00,
+        img: "/momos.png",
+        name: "Momos",
+        description: "Steamed dumplings filled with savory minced veggies, served with a spicy chili dipping sauce.",
+        price: 50.00
+    },
+    {
+        discount: 5,
+        img: "/pancake.png",
+        name: "Pancakes",
+        description: "Fluffy, golden pancakes stacked and drizzled with syrup, perfect for breakfast or dessert.",
+        price: 12.20
+    },
+    {
+        discount: 6,
+        rating: 4.00,
+        img: "/honey-bread.png",
+        name: "Honey Bread",
+        description: "Soft and sweet bread infused with rich honey flavor — great for snacking or toasting.",
+        price: 15.00
+    },
+    {
+        discount: 17,
+        rating: 4.00,
+        img: "/tikki-burger.png",
+        name: "Aloo Tikki Burger",
+        description: "Crispy potato patty spiced with Indian flavors, tucked in a bun with chutneys and veggies.",
+        price: 50.00
+    },
+    {
+        discount: 10,
+        img: "/salad.png",
+        name: "Green Salad",
+        description: "A refreshing mix of leafy greens, cucumbers, tomatoes, and olives tossed in vinaigrette.",
+        price: 45.00
+    }
+];
